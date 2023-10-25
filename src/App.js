b21882cdabea5d448fad03259c873cdbadea32f1
i456919 <joe.lin456919@gmail.com>
2023-10-25 22:21:32 +0800

    提交範例

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import { Renderer } from "@vivliostyle/react";
 
 function App() {
+  const [page, setPage] = React.useState(1);
+
+  function next() {
+    setPage((page) => page + 1);
+  }
+
+  function prev() {
+    setPage((page) => page - 1);
+  }
+
+  function onLoad(state) {
+    console.log(state.epageCount, state.docTitle);
+  }
+
+  //https://github.com/IDPF/epub3-samples/tree/master/30/accessible_epub_3
+  //http://127.0.0.1:5500
+  const sample =
+    "https://github.com/YunTaoLin/epub_example/";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Renderer source={sample} page={page} onLoad={onLoad} bookMode={true}  />
+      <button onClick={prev}>Prev</button>
+      <button onClick={next}>Next</button>
     </div>
   );
 }
